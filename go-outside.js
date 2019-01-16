8d51800ca43761bc82b09284dbbb26a5bdb181c6
MattTurnip <mattadavey@gmail.com>
2019-01-16 15:38:15 +0000

    adds conditional excercises

---
+++ go-outside.js
@@ -0,0 +1,13 @@
+var temperature = 35;
+
+if (temperature < 0) {
+  console.log("Make sure to pack a scarf");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it");
+} else if (temperature > 50) {
+  console.log("Wear a heat resistant suit!");
+} else {
+  console.log("short sleeves are fine");
+}
+
+console.log("Now you're ready to go outside!");
